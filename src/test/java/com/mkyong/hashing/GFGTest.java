694f6a5e1df6e238df44cf7ea5e31f8d551f778c
vidya1234 <vidyatmgowda@gmail.com>
2019-06-13 17:16:52 +0530

    added

---
+++ src/test/java/com/mkyong/hashing/GFGTest.java
@@ -0,0 +1,38 @@
+package com.mkyong.hashing;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GFGTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public GFGTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( GFGTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testGFG()
+    {
+        assertTrue( true );
+    }
+}
